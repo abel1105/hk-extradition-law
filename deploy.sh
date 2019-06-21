260cf7ee72896470a5aacf3b7a165665a82fd29b
Abel Lee <kent62001@gmail.com>
2019-06-21 16:08:52 +0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:abel1105/hk-extradition-law.git master:gh-pages
+git push -f "https://${GITHUB_TOKEN}@github.com/abel1105/hk-extradition-law.git" master:gh-pages
 
 cd -

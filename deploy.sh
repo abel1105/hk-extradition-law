03d106da92957d66e1cc6695c7ff3108c728408f
Abel Lee <kent62001@gmail.com>
2019-06-21 16:29:01 +0800

    change deploy provider to pages

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # abort on errors
 set -e
 
+# build
+npm run build
+
 # navigate into the build output directory
 cd dist
 
@@ -11,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f "https://${GITHUB_TOKEN}@github.com/abel1105/hk-extradition-law.git" master:gh-pages
+git push -f git@github.com:abel1105/hk-extradition-law.git master:gh-pages
 
 cd -
